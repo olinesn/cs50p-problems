f536e0f946f74703e3308a57d9609a51c9600bee
olinesn <olinesn@gmail.com>
2022-07-17 21:26:21 -0400

    first update to file

--- week0/hello.py
+++ week0/hello.py
@@ -1,3 +1,3 @@
 # Ask user for their name, 'strip' the whitespaces & capitalize it
-name = input("What is your name?: ").strip().title()
+name = input("What is your name?: ")
 print("Hello, " + name + ".")
